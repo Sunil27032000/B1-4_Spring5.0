a5954c8b1cc5fd112c6346187308b4e828fd924b
sunilkumar <sunilkumar@LAPTOP-JOC7RKCP>
2022-06-29 03:11:50 +0530

    First Commit

---
+++ SpringDependencyInjection/src/main/java/di1/Cheat.java
@@ -0,0 +1,14 @@
+package di1;
+
+public class Cheat {
+	public Cheat() {
+		super();
+		System.out.println("Ready to execute");
+	}
+
+	void mathCheat()
+	{
+		System.out.println("Microxerox is ready to pass");
+	}
+
+}
---
+++ SpringDependencyInjection/src/main/java/di1/Student1.java
@@ -0,0 +1,19 @@
+package di1;
+
+public class Student1 {
+	Cheat c;
+	
+
+
+	public void setC(Cheat c) {
+		this.c = c;
+	}
+
+
+
+	public void cheating()
+	{
+		c.mathCheat();
+	}
+
+}
