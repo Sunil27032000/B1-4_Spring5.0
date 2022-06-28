b50747864ad4f02e3dae0e9c8604a607c0484d86
sunilkumar <sunilkumar@LAPTOP-JOC7RKCP>
2022-06-29 03:12:53 +0530

    Firat Commit

---
+++ SpringMVC/src/main/java/example/springstarter/HomeController.java
@@ -0,0 +1,14 @@
+package src.main.java.example.springstarter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+	@RequestMapping("Sunil")
+	public String home()
+	{
+		System.out.println("Welcome to TechnoServe");
+		return "home.jsp";
+	}
+
+}
